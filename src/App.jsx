8611590a0af7ad10aa12a0444c48679ea6f5eb2a
Saakshi Gupta <saakshigupta7777@gmail.com>
2025-08-04 15:14:32 +0530

    homepage and login skeleton

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login'; // move your login component here
+import Navbar from './components/Navbar'; 
+
+function App() {
+  return (
+    <Router>
+       <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
